8e162bfbe8e3bab8621187db8da32e79c5fd3545
Nikita Gorbachev <nikita_98_gorbachew@mail.ru>
2022-08-17 13:45:14 +0300

    Number5

---
+++ Number5/Program.cs
@@ -0,0 +1,23 @@
+﻿/*Напишите программу, которая на вход принимает одно число (N), а на выходе показывает все целые числа в промежутке от -N до N.
+4 -> "-4, -3, -2, -1, 0, 1, 2, 3, 4" 
+2 -> " -2, -1, 0, 1, 2"
+*/
+
+Console.Write("Введите число N: ");
+
+int number = Convert.ToInt32(Console.ReadLine()); 
+// -N = N*(-1)
+// [-N ; N] -> [negativeNumber; number]
+if (number < 0)
+{
+    number*=(-1); // -5* (-1) = 5 - N
+}
+int negativeNumber = number * (-1);
+while (negativeNumber <= number) // пока справделиво условие
+{
+   Console.WriteLine(negativeNumber);
+   negativeNumber++;  //negativeNumber += инкремент: negativeNumber = negativeNumber + 1
+
+}
+    
+
