fdaf42955dd463c39278e53b5189f7876426f963
Nikita Gorbachev <nikita_98_gorbachew@mail.ru>
2022-08-17 14:00:42 +0300

    Правки

--- Number26/Program.cs
+++ Number26/Program.cs
@@ -2,7 +2,7 @@
 // 456 -> 3
 // 78 -> 2
 // 89126 -> 5
-/*
+
 int num = Convert.ToInt32(Console.ReadLine());
 int GetCountNumbers(int number)
 {
@@ -16,50 +16,7 @@ int GetCountNumbers(int number)
 }
 
 Console.WriteLine($"Количество чисел: {GetCountNumbers(num)}");
-*/
-
-// Задача 28: Напишите программу, которая принимает на вход число N и выдаёт произведение чисел от 1 до N.
-// 4 -> 24 
-// 5 -> 120
 
 
-/*
-int num = Convert.ToInt32(Console.ReadLine());
-
-int GetMultiply (int number)
-{
-    int result = 1; // 2*0 = 2; при умножении указываем 1: любое число на 1 = число
-    // Multiply[1;N]
-    for (int i = 1; i <= number; i++)
-    {
-        result *= i;
-    }
-    return result;
-}
-System.Console.WriteLine($"Факториал от {num} = {GetMultiply(num)}");
-*/
 
 
-// Задача 30: Напишите программу, которая выводит массив из 8 элементов, заполненный нулями и единицами в случайном порядке.
-// [1,0,1,1,0,1,0,0]
-
-
-// int[] array = new int[8];
-// Random x = new Random();
-// for (int i = 0; i < 8; i++)
-// {
-//     array[i] = x.Next(0, 2);
-//     Console.Write(array[i] + " ");
-// }
-
-int[] GetBinaryArray(int size)
-{
-    int[] result = new int[size]; // size = 2; result = [0,0]
-    for (int i = 0; i < result.Length; i++) // size = result.Length
-    {
-        result[i] = new Random().Next(2); // если указано одно число, то идем от 0 до этого числа
-    }
-    return result;
-}
-int[] testArray = GetBinaryArray(8);
-Console.WriteLine($"Массив: [ {String.Join("; ",testArray)} ]");
