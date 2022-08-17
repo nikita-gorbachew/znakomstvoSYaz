904fb1bda2f6399fa9a5640efc4a0865b4b15ec5
Nikita Gorbachev <nikita_98_gorbachew@mail.ru>
2022-08-17 13:45:35 +0300

    Number9

---
+++ Number9/Program.cs
@@ -0,0 +1,22 @@
+﻿//Напишите программу, которая выводит случ число из отрезка 
+//[10, 99] и показывает наибольшую цифру числа.
+// 78 -> 8
+//12 -> 2
+//85 -> 8
+
+// 78 -> 7(нацело 78 на цело на 10) 78/10 = 7
+//78 -Ю 8(остаток от деления на 10) -> 78 % 10
+
+int number = new Random().Next(10, 100);
+//Console.WriteLine($"Рандомное число: {number}"); //Интерполяция "$"
+//Console.WriteLine("Рандомное число" + number); // Конкатенация - сложение строк
+//camelCase
+
+int firstDigit = number / 10;
+int secondDigit = number % 10;
+
+int maximum = firstDigit; //пусть максимум -  это 1 число (количество десятков)
+if (maximum < secondDigit) maximum = secondDigit;
+
+Console.WriteLine($"Максимальная цифра в числе {number} - это {maximum}");
+//Console.WriteLine($"Текст: {В фигурных скобках - переменная}")
