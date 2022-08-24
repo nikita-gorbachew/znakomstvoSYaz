3049173573cc09f3e9ca8f128ed617125ab1b87f
Nikita Gorbachev <nikita_98_gorbachew@mail.ru>
2022-08-24 11:35:13 +0300

    Number45

---
+++ Number45/Program.cs
@@ -0,0 +1,12 @@
+﻿// Задача 45: Напишите программу, которая будет создавать копию заданного
+//  массива с помощью поэлементного копирования.
+int[] CopyArray(int[] inArray)
+{
+int[] result = new int[inArray.Length];
+for (int i = 0; i < inArray.Length; i++)
+{
+result[i] = inArray[inArray.Length - 1 - i];
+
+}
+return result;
+}
