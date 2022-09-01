ee09235b966c246c5ed8c9e3d4683618d00a9034
Nikita Gorbachev <nikita_98_gorbachew@mail.ru>
2022-09-01 21:41:01 +0300

    Number67

---
+++ Number67/Program.cs
@@ -0,0 +1,12 @@
+﻿// Задача 67: Напишите программу, которая будет принимать на вход 
+//число и возвращать сумму его цифр.
+// 453 -> 12
+// 45 -> 9
+
+int number = Convert.ToInt32(Console.ReadLine());
+int SumNumber(int number)
+{
+    if (number == 0) return 0;
+    return ( number % 10 + SumNumber(number / 10));
+}
+Console.Write($"Сумма цифр в числе {number} = {SumNumber(number)}");
