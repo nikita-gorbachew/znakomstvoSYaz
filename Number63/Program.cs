7812df907cd9b47587a33abe7b2b079ac1885b37
Nikita Gorbachev <nikita_98_gorbachew@mail.ru>
2022-09-01 21:40:22 +0300

    Number63

---
+++ Number63/Program.cs
@@ -0,0 +1,18 @@
+﻿// Задача 63: Задайте значение N. Напишите программу, 
+// которая выведет все натуральные числа в промежутке от 1 до N.
+// N = 5 -> "1, 2, 3, 4, 5"
+// N = 6 -> "1, 2, 3, 4, 5, 6"
+
+int N = int.Parse(Console.ReadLine());
+
+// int.Parse - переделывает строчку в число: "null" - ссылка на пустоту 
+// Convert.ToInt32()
+
+/// int start - начало (1), end - число N
+string PrintNumbers(int start, int end)
+{
+    if (start == end) return start.ToString(); // Базовый случай
+    return (start + ", " + PrintNumbers(start + 1, end)); // Рекурсивный случай
+}
+
+Console.WriteLine(PrintNumbers(1, N));
