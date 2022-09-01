6e9d9f1c3640272d123fe8457bc6b68a79032a7c
Nikita Gorbachev <nikita_98_gorbachew@mail.ru>
2022-09-01 21:40:52 +0300

    Number65

---
+++ Number65/Program.cs
@@ -0,0 +1,16 @@
+﻿// Задайте значения M и N. Напишите программу, которая выведет все натуральные числа в промежутке от M до N.
+// M = 1; N = 5 -> "1, 2, 3, 4, 5"
+// M = 4; N = 8 -> "4, 6, 7, 8"
+
+int M = Convert.ToInt32(Console.ReadLine());
+int N = int.Parse(Console.ReadLine());
+if (N > M) return;
+
+
+string PrintNumbers(int start, int end)
+{
+    if (start == end) return start.ToString(); // Базовый случай
+    return (start + ", " + PrintNumbers(start + 1, end)); // Рекурсивный случай
+}
+
+Console.WriteLine(PrintNumbers(M, N));
